ffc58c61ee9065c724810be6c285564237ffd027
Pierre Keda <pierrekeda83@gmail.com>
2021-06-23 02:38:38 +0200

    Cleanup template-app

--- README.md
+++ README.md
@@ -1,21 +1,3 @@
-# Keda's App Template
+# Textformer
 
-v.0.2.0  
-  
-Setup for webpack + babel + github
-
-Quick start :
-
-1. Copy folder
-2. Change name in package.json, dist/index.html
-3. Create project on github
-4. Open terminal in root
-5. Run:
-```
-npm install
-git init
-git remote add origin https://github.com/pierrekeda/app.git
-git push -u origin master
-```
-  
-6. Get it done !
+Text data transformation utility
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,12 @@ let config = {
 
 	entry: './src/main.js',
 
+	output: {
+		filename: 'main.js',
+		path: path.resolve( __dirname, './dist' ),
+		publicPath: '/js/'
+	}
+
 };
 
 module.exports = ( env, argv ) => {
@@ -12,15 +18,10 @@ module.exports = ( env, argv ) => {
 
 		config.mode = 'development';
 
-		config.output = {
-			filename: 'main.js',
-			path: path.resolve( __dirname, './dist/' )
-		};
-
 		config.devtool = 'inline-source-map';
 
 		config.devServer = {
-			contentBase: './dist/',
+			contentBase: './dist',
 			host: '192.168.1.10',
 			port: 8080,
 			disableHostCheck: true,
@@ -29,11 +30,6 @@ module.exports = ( env, argv ) => {
 	} else {
 
 		config.mode = 'production';
-
-		config.output = {
-			path: path.resolve( __dirname, './dist/' ),
-			filename: 'main.js'
-		};
 
 		config.module = {
 			rules: [
@@ -46,12 +42,6 @@ module.exports = ( env, argv ) => {
 					}
 				}
 			]
-		};
-
-		config.externals = {
-			animejs: 'anime',
-			'dat.gui': 'dat.gui',
-			three: 'THREE'
 		};
 
 	}
